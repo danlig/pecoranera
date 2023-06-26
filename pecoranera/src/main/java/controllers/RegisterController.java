625b5a46eab601cda59a193ab5507b2f82118110
jose-sgariglia <josesugariria@gmail.com>
2023-06-26 12:25:53 +0200

    First Release Registration
    Prima versione della registrazione utente.
    - Manca la criptazione della password
    - Troppe chiamate al db
    - Pagina di prova

--- pecoranera/src/main/java/controllers/RegisterController.java
+++ pecoranera/src/main/java/controllers/RegisterController.java
@@ -2,13 +2,22 @@ package controllers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.CartDao;
+import dao.TagDao;
+import dao.UserDao;
+import model.Cart;
+import model.Tag;
+import model.User;
 
 /**
  * Servlet implementation class RegisterController
@@ -35,12 +44,61 @@ public class RegisterController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		List<String> errors = new ArrayList<>();
-		String email =
+		String username = request.getParameter("username");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String conf_password = request.getParameter("conf_password");
 		
+		Set<Tag> tags = new HashSet<>();
+		for (Tag tag : TagDao.doRetrieveAll()) {
+			if (request.getParameter(tag.getId() + "-tag") != null) {
+				tags.add(tag);
+			}
+		}
+
+		List<User> users = UserDao.doRetrieveAll();
+		
+		if (email == null || email.trim().equals("")) {
+			errors.add("Insert email<br>");
+		} else if (users.stream().filter(p -> p.getEmail().equals(email)).count() != 0) {
+			errors.add("Existing email<br>");
+		}
+		
+		if (username == null || username.trim().equals("")) {
+			errors.add("Insert username<br>");
+		} 
+		
+		if (password == null || password.trim().equals("")) {
+			errors.add("Insert password<br>");
+		} else if (!password.equals(conf_password)) {
+			errors.add("Not equeals passwords<br>");
+		}
+		
+		if (!errors.isEmpty()) {
+			request.setAttribute("errors", errors);
+		} else {
+			User user = new User();
+				
+			user.setUsername(username);
+			user.setEmail(email);
+			user.setPassword(password);
+			user.setAdmin(false);
+			user.setTags(tags);
+			UserDao.doSave(user);
+			
+			Cart cart = new Cart();
+			cart.setUser(
+					UserDao.doRetrieveAll().stream().filter(u -> u.getEmail().equals(email)).findFirst().get()
+					);
+			CartDao.doSave(cart);
+			
+			
+			request.setAttribute("message", "tutt appost");
+		}
 		
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/register.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
--- pecoranera/src/main/webapp/register.jsp
+++ pecoranera/src/main/webapp/register.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+    pageEncoding="ISO-8859-1" import="dao.TagDao, model.Tag" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page isELIgnored="false" %>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,32 +9,40 @@
 		<title>Register</title>
 	</head>
 	<body>
+		<%@ page language="java" import="java.util.List" %>
 		<%
-		String email = (String)request.getAttribute("email");
-		if(email == null) email = "";
-		
-		String password = (String)request.getAttribute("password");
-		if(password == null) password = "";
-		
-		
-		String error = (String)request.getAttribute("error");
-		if(error != null) {
+			String message = (String)request.getAttribute("message");
+			if(message != null) {
 		%>
-		<div class="error"><%=error %></div>
+			<div style="color: green"><%= message %></div>
 		<%
-		}
-		
-		String message = (String)request.getAttribute("message");
-		if(message != null) {
-		%>s
-		<div class="message"><%=message %></div>
+		  	}
+			
+			List<String> errors = (List<String>) request.getAttribute("errors");
+			if (errors != null)
+				for (String error : errors) {
+		%>
+			<p style="color: red"><%= error %></p>
 		<%
-		  }
+				}
 		%>
 		
 		<form name="checkformname" method="POST" action="RegisterController">
-			Email: <input type="email" name="name" placeholder="Your name" value="<%=email %>"><br>
-			Password: <input type="text" name="password" placeholder="Your surname" value="<%=password %>"><br>
+			Username: <input type="text" name="username" placeholder="Your name"><br>
+			Email: <input type="email" name="email" placeholder="Your name"><br>
+			Password: <input type="password" name="password" placeholder="Your surname"><br>
+			Confirm Password: <input type="password" name="conf_password" placeholder="Your surname"><br>
+			
+			Tags:<br>
+			<%
+				List<Tag> tags = TagDao.doRetrieveAll();
+				
+				for (Tag tag : tags) {
+			%>
+				<label for = "<%= tag.getId() %>"><%= tag.getName() %></label>
+				<input id = "<%= tag.getId() %>-tag" type="checkbox" name="<%= tag.getId() %>-tag" value="true">
+				
+			<%} %>
 			<br>
 			<input type="submit">
 			<input type="reset">
