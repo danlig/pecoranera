286137b5a6ae019e9679339a3f0d0dc82136db80
KronosPNG <turcoluigi@outlook.com>
2023-07-11 07:00:36 +0200

    mistake

---
+++ pecoranera/src/main/webapp/scripts/admin/orders.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    let orderToHTML= function(order){
+        return `<div id="${order.id}" class="order">
+                    <a class="event-link" href="event_details.jsp?id=${order.event.id}">
+                        <img class="event-img" src="" alt="">
+                    </a>
+            
+                    <div>
+                        <div class="order-details">
+                            <h3 class="order-header">${order.event.name}</h3>
+                            <h4 class="event-date">Data evento: ${new Date(Date.parse(order.event.date)).toLocaleDateString("it-IT", {day:"2-digit", month:"2-digit", year:"numeric"})}</h4>
+                            <p class="order-date">Data ordine: ${new Date(Date.parse(order.date)).toLocaleDateString("it-IT", {day:"2-digit", month:"2-digit", year:"numeric"})}</p>
+                        </div>
+                
+                        <div class="ticket-data">
+                            <span>Numero biglietti: ${order.tickets} </span>
+                            
+                            <span class="price">€ <span>${(parseFloat(order.price) * parseInt(order.tickets)).toFixed(2)}</span></span>
+                
+                        </div>
+                    </div>
+    
+                    <img class="ticket-qr" src="https://chart.googleapis.com/chart?cht=qr&chs=450x450&chl=${user}+${order.event.id}+${order.event.date}" alt="">
+    
+                </div>`;
+    }
+    
+    let loadUserOrder = async function(offsetPage){
+        return $.ajax({
+            url: "admin/order/list",   
+    
+            success: (orders) => {
+                $.each(orders, function(key, order) {
+                    $("#order-list").append(orderToHTML(order));
+                });
+                
+            }
+        });
+    }
+    
+    loadUserOrder();
+})
